b1ba493dbbc701b5c14b171b9079a813066cc9b2
Konstantin8881888 <kamagoroff@gmail.com>
2023-04-27 06:14:58 +0300

    Initial Structure
    Initial Structure

---
+++ src/main/java/com/example/TextGrabber/TextGrabberApplication.java
@@ -0,0 +1,13 @@
+package com.example.TextGrabber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextGrabberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextGrabberApplication.class, args);
+	}
+
+}
